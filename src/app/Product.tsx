c5fdf8605087dd8c9291d7b1a26eadc6747e9dfd
Matheus Muruci <matheusmuruci@outlook.com>
2026-03-18 16:23:10 -0300

    Adicionado primeiro tipo de navegação: Stack

---
+++ src/app/Product.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { ButtonIcon } from "@/components/ButtonIcon";
+import { Header } from "@/components/Header";
+import { Title } from "@/components/Title";
+
+export function Product() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
+      <Header>
+        <ButtonIcon name="arrow-circle-left" onPress={() => navigation.goBack()} />
+        <Title>Product</Title>
+      </Header>
+    </View>
+  );
+}
