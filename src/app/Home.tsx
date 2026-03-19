2b884e9e667d23bdcca8f126fe69e6648a057903
Matheus Muruci <matheusmuruci@outlook.com>
2026-03-19 16:31:02 -0300

    Adicionado navegação bottom tabs ao projeto

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -5,8 +5,9 @@ import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 
 import { StackRoutesProps } from "@/routes/StackRoutes";
+import { BottomRoutesProps } from "@/routes/BottomRoutes";
 
-export function Home({ navigation }: StackRoutesProps<"home">) {
+export function Home({ navigation }: BottomRoutesProps<"home">) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
