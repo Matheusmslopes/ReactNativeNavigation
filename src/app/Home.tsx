b27351cd3b7780f19d8ba0ea58e3a0f501854f8f
Matheus Muruci <matheusmuruci@outlook.com>
2026-03-19 16:55:32 -0300

    Adicionado navegação Drawer ao projeto

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -6,11 +6,13 @@ import { Title } from "@/components/Title";
 
 import { StackRoutesProps } from "@/routes/StackRoutes";
 import { BottomRoutesProps } from "@/routes/BottomRoutes";
+import { DrawerRoutesProps } from "@/routes/DrawerRoutes";
 
-export function Home({ navigation }: BottomRoutesProps<"home">) {
+export function Home({ navigation }: DrawerRoutesProps<"home">) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
+        <ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()} />
         <Title>Home</Title>
         <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: "1" })} />
       </Header>
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -6,13 +6,15 @@ import { Title } from "@/components/Title";
 
 import { StackRoutesProps } from "@/routes/StackRoutes";
 import { BottomRoutesProps } from "@/routes/BottomRoutes";
+import { DrawerRoutesProps } from "@/routes/DrawerRoutes";
 
-export function Product({ navigation, route }: BottomRoutesProps<"product">) {
+export function Product({ navigation, route }: DrawerRoutesProps<"product">) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <ButtonIcon name="arrow-circle-left" onPress={() => navigation.goBack()} />
         <Title>Product {route.params?.id}</Title>
+        {/*<ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()}/> */}
       </Header>
     </View>
   );
